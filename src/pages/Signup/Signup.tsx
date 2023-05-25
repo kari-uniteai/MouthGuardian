1461f1ba904fb7313b1c618f21cc0419c349bd11
Aleksi Nokela <aleksi.nokela@younite-ai.com>
2023-05-25 15:43:57 +0300

    username and id saved to firebase

--- src/pages/Signup/Signup.tsx
+++ src/pages/Signup/Signup.tsx
@@ -1,38 +1,49 @@
-import React, { useState } from 'react';
-import { firebase } from '../../services/firebase.config.js';
-import classes from './Signup.module.css';
+import { useState } from 'react';
+import { getAuth, createUserWithEmailAndPassword } from 'firebase/auth';
+import { getDatabase, ref, push, set } from 'firebase/database';
 import { useNavigate } from 'react-router-dom';
-import { getDatabase } from 'firebase/database';
+import classes from './Signup.module.css';
 
 const Signup = () => {
     const [userName, setUserName] = useState('');
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
     const [error, setError] = useState('');
-    const database = getDatabase();
     const navigate = useNavigate();
 
     const handleSignup = (e: React.FormEvent) => {
         e.preventDefault();
-
+      
         if (email.trim() === '' || password.trim() === '' || userName.trim() === '') {
-            setError('Please enter email and password and username');
-            return;
+          setError('Please enter email, password, and username');
+          return;
         }
-        firebase
-            .auth()
-            .createUserWithEmailAndPassword(email, password)
-            .then((userCredential) => {
-                // User signed up successfully
-                const user = userCredential.user;
-                console.log(user);
-                navigate('/dashboard');
-            })
-            .catch((error) => {
-                // Handle signup error
-                setError(error.message);
+      
+        const auth = getAuth();
+        const database = getDatabase();
+      
+        createUserWithEmailAndPassword(auth, email, password)
+          .then((userCredential) => {
+            // User signed up successfully
+            const user = userCredential.user;
+            const userId = user?.uid;
+      
+            // Save the username to the Realtime Database
+            const usersRef = ref(database, 'users');
+            const newUserRef = push(usersRef);
+            set(newUserRef, {
+              username: userName,
+              userid: userId,
             });
-    };
+      
+            console.log(user);
+            navigate('/dashboard');
+          })
+          .catch((error) => {
+            // Handle signup error
+            setError(error.message);
+          });
+      };
 
     return (
         <div className={classes.container}>
