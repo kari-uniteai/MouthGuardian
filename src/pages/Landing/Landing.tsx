30b07ed52d8110f9b3367e7fcf42c010917084f6
Sami Mustonen <sami.mustonen@finlabs.com>
2023-05-26 11:23:40 +0300

    landing page updated

--- src/pages/Landing/Landing.tsx
+++ src/pages/Landing/Landing.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
-import logoImage from '../../images/logo.png';
-import image2 from '../../images/placeholderlogo.png';
+import Logo from '../../images/logo.png';
+import image2 from '../../images/tooth.png';
 import classes from './Landing.module.css';
 
 const Landing = () => {
@@ -17,18 +17,25 @@ const Landing = () => {
 
     return (
         <div className={classes.container}>
-            <div className={classes.logo}>
-                <img src={logoImage} alt="Logo" />
+            <div className={classes.top}>
+                <img
+                    src={Logo}
+                    className={classes.logo}
+                />
             </div>
-            <div className={classes.title}>Mouth Guardian</div>
-            <div className={classes.description}>Puhtaat hampaat, parempi mieli!</div>
-            <img
-                src={classes.image2}
-                alt="Image2"
-            />
-            <div className={classes.buttonRow}>
-                <button onClick={handleLoginClick}>Login</button>
-                <button onClick={handleSignupClick}>Signup</button>
+            <div className={classes.bottom}>
+                <img
+                    className={classes.image2}
+                    src={image2}
+                    alt="Image2"
+                />
+                <div className={classes.title}>Mouth Guardian</div>
+                <div className={classes.description}>Reimagine Dental Care, Embrace the Power of Innovation!</div>
+
+                <div className={classes.buttonRow}>
+                    <button onClick={handleLoginClick}>Login</button>
+                    <button onClick={handleSignupClick}>Signup</button>
+                </div>
             </div>
         </div>
     );
